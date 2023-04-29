417abf1780e41968972add95d0f5352a9f6e6fc4
Devul7178 <danahar2@illinois.edu>
2023-04-29 01:47:12 -0500

    changed to better program

--- mini-rv32ima/bintoh.c
+++ mini-rv32ima/bintoh.c
@@ -1,2 +1,24 @@
 #include <stdio.h>
-int main(int argc,char ** argv) {if(argc==1) return -1; int c, p=0; printf( "static const unsigned char %s[] = {", argv[1] ); while( ( c = getchar() ) != EOF ) printf( "0x%02x,%c", c, (((p++)&15)==15)?10:' '); printf( "};" ); return 0; }
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        printf("Usage: %s <dtb_file>\n", argv[0]);
+        return -1;
+    }
+
+    FILE* file = fopen(argv[1], "rb");
+    if (file == NULL) {
+        printf("Failed to open file: %s\n", argv[1]);
+        return -1;
+    }
+
+    int c, p = 0;
+    printf("static const unsigned char default64mbdtb[] = {");
+    while ((c = fgetc(file)) != EOF) {
+        printf("0x%02x,%c", c, (((p++) & 15) == 15) ? 10 : ' ');
+    }
+    printf("};");
+
+    fclose(file);
+    return 0;
+}
